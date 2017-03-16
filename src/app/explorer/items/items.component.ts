460effd39171af6cca93fe36943453d919ff406f
niko <s4nigill@uni-trier.de>
2017-03-16 23:55:35 +0100

    mockup table

--- src/app/explorer/items/items.component.ts
+++ src/app/explorer/items/items.component.ts
@@ -1,16 +1,41 @@
 import {Component, OnInit} from '@angular/core';
+import {RestService} from '../rest.service'
+import {SelectItem} from 'primeng/primeng';
+import {Item} from "../models/Item";
 
 @Component({
   selector: 'app-items',
   templateUrl: 'items.component.html',
-  styleUrls: ['items.component.scss']
+  styleUrls: ['items.component.scss'],
+  providers: [RestService]
 })
-export class ItemsComponent implements OnInit {
 
-  constructor() {
+export class ItemsComponent implements OnInit {
+  constructor(private restService: RestService) {
+  }
+
+  items: Item[];
+  errorMessage: string;
+  message: string;
+
+  getItems() {
+    this.restService.getItems()
+      .subscribe(
+        items => this.items = items,
+        error => this.errorMessage = <any>error);
+  }
+
+  onClick() {
+    this.getItems();
+    this.message = JSON.stringify(this.items);
+    console.log("Clicked");
+    console.log(this.items);
   }
 
   ngOnInit() {
+    this.getItems();
+    this.message = JSON.stringify(this.items);
+    console.log(this.items);
   }
 
 }
--- src/app/explorer/models/Item.ts
+++ src/app/explorer/models/Item.ts
@@ -1,3 +1,15 @@
-/**
- * Created by niko on 16.03.17.
- */
+import {Injectable} from  "@angular/core";
+
+
+export interface Item {
+  id: number;
+  name: string;
+  department: string;
+  description: string;
+  gross_price: string;
+  height_unit: string;
+  manufacturer: string;
+  rent: string;
+  serialnumber: string;
+  weight: string;
+}
